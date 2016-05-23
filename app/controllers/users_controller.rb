82e12ec1821399cab9b124db78f7d89e04b7b1bf
SOPHIE DEBENEDETTO <sophie.debenedetto@gmail.com>
2016-05-23 12:20:59 -0400

    return empty json object on successful user registration ember ajax request expects response type JSON or throws a JSON parse error. meh

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      head :ok
+      render json: {}, status: 200
     else
       errors = user.generate_json_api_error
       render json: errors, status: 422
