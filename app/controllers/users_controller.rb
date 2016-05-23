41a40bd334fb48a0094292b02f788459af3809d0
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2016-05-22 20:57:34 -0400

    structure error object to be JsonApi compliant; need to make error generation dynamic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,14 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate, only: [:create]
+  # skip_before_action :authenticate, only: [:create]
   def index
     render json: User.all
   end
 
   def show
-    render json: User.find_by(params[:id])
+    render json: User.find(params[:id])
   end
 
   def create
-    binding.pry
     user = User.new(user_params)
     if user.save
       head :ok
@@ -17,15 +16,30 @@ class UsersController < ApplicationController
       errors = {
         "errors": [
           {
-            "status": "400",
-            "title": "registration failure",
-            "detail": "#{user.errors.messages}"
+            "detail": "email isn't valid",
+            "source": {
+              "pointer": "user/email"
+            }
+          },
+          {
+            detail: "pasword and password confirmation don't match",
+            source: {
+              "pointer": "user/password"
+            }
           }
         ]
       }
-      binding.pry
 
-      render json: errors, status: 400
+      # errors = {
+      #   "errors": [
+      #     {
+      #       "status": "400",
+      #       "title": "registration failure",
+      #       "detail": user.errors.messages
+      #     }
+      #   ]
+      # }
+      render json: errors, status: 422
     end
   end
 
@@ -37,4 +51,25 @@ class UsersController < ApplicationController
 
 
 end
+
+
+
+
+
+# {
+#   "errors": [
+#     {
+#       "detail": "email isn't valid",
+#       "source": {
+#         "pointer": "data/attributes/email"
+#       }
+#     },
+#     {
+#       detail: "pasword and password confirmation don't match",
+#       source: {
+#         "pointer": "data/attributes/password"
+#       }
+#     }
+#   ]
+# }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
 end
